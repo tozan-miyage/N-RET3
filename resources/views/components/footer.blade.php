676ab3ab48a392d545fafb21f7e40608c2d59ce1
tozan-miyage <sapporogentaro@yahoo.co.jp>
2020-09-17 06:37:50 +0000

    backup

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,8 @@
     <div class="row footer_area">
         <div class="col-12">
             <p>
-                Copyright 2020 Gen Miyazato All rights reserved.
-                <a href="#">利用規約</a><a href="#"> プライバシーポリシー</a>
+                Copyright 2020 N-RET Secretariat All rights reserved.
+                <!--<a href="#">利用規約</a><a href="#"> プライバシーポリシー</a>-->
             </p>
         </div>
     </div>
